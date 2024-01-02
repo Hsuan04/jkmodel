bca267ea7bba42f90d6039eb40f864adb693bc5a
lawrence <haohsuan861104@gmail.com>
2024-01-02 13:14:54 +0800

    完成後台模板與連結設定 1/2 13:14

--- jkmodel/src/main/resources/static/js/shop.js
+++ jkmodel/src/main/resources/static/js/shop.js
@@ -23,36 +23,46 @@ $(function () {
                 var isNewProduct = daysDifference <= 14;
                 var isSaleProduct = daysDifference >= 90;
 
+                //生成圖片
+                var photosContainer = '';
+                var imageUrl = 'data:image/jpeg;base64,' + product.firstPhotoString;
+                    photosContainer += `<img src="${imageUrl}" alt="Product Photo">`;
+
+
+
                 // 動態產生商品卡片的HTML
                 var productCard = `
-    <div class="col-lg-4 col-md-6 col-sm-6">
-        <div class="product__item">
-            <div class="product__item__pic set-bg" data-setbg="img/product/product-2.jpg">
-                ${isNewProduct ? '<span class="label new" style="font-size: 14px; color: red; border: 1px solid red;" name="new">最新推出</span>' : ''}
-                ${isSaleProduct ? '<span class="label" name="sale" style="font-size: 14px; color: white; background-color: black; border: 1px solid;">特價中</span>' : ''}
-                <ul class="product__hover">
-                    <li><a href="#"><img src="../static/images/icon/heart.png" alt=""></a></li>
-                    <li><a href="shopDetails.html?productNo=${product.productNo}"><img src="../static/images/icon/search.png" alt=""></a></li>
-                </ul>
-            </div>
-            <div class="product__item__text">
-                <h6>
-                    <span name="productName">${product.name}</span>
-                    （
-                    <span name="score">${product.score}</span>
-                    分）
-                </h6>
-                ${isSaleProduct ?
-                    `<span name="price" style="text-decoration-line: line-through;">${product.price} 元</span>
-                    <span style="color: red; margin-left: 10px;">
-                        <span name="salePrice">${Math.round(product.price * 0.9)}</span>
-                        <span>元</span>
-                    </span>` :
-                    `<span name="price">${product.price} 元</span>`}
-                <a href="#" class="add-cart">+ 加入購物車</a>
-            </div>
-        </div>
-    </div>`;
+                    <div class="col-lg-4 col-md-6 col-sm-6">
+                        <div class="product__item">
+                            <div class="product__item__pic set-bg" data-setbg="img/product/product-2.jpg">
+                                <div class="product__item__photos">
+                                    ${photosContainer}
+                                </div>
+                               ${isNewProduct ? '<span class="label new" style="font-size: 14px; color: red; border: 1px solid red;" name="new">最新推出</span>' : ''}
+                                ${isSaleProduct ? '<span class="label" name="sale" style="font-size: 14px; color: white; background-color: black; border: 1px solid;">特價中</span>' : ''}
+                                <ul class="product__hover">
+                                  <li><a href="#"><img src="../static/images/icon/heart.png" alt=""></a></li>
+                                  <li><a href="shopDetails.html?productNo=${product.productNo}"><img src="../static/images/icon/search.png" alt=""></a></li>
+                               </ul>
+                          </div>
+                        <div class="product__item__text">
+                                <h6>
+                                  <span name="productName">${product.name}</span>
+                                   （
+                                   <span name="score">${product.score}</span>
+                                   分）
+                             </h6>
+                              ${isSaleProduct ?
+                                 `<span name="price" style="text-decoration-line: line-through;">${product.price} 元</span>
+                                    <span style="color: red; margin-left: 10px;">
+                                     <span name="salePrice">${Math.round(product.price * 0.9)}</span>
+                                     <span>元</span>
+                                 </span>` :
+                                 `<span name="price">${product.price} 元</span>`}
+                               <a href="#" class="add-cart">+ 加入購物車</a>
+                             </div>
+                        </div>
+                 </div>`;
                 // 將商品卡片插入到容器中
                 $("#productContent").append(productCard);
             });
